3c17fe23added2929af6da26e8e660a7d3527c85
Ryan Smith <ryan.smith1@bp.com>
2022-12-12 13:22:06 +0000

    Replace matchmedia workaround with polyfill

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,13 +1,9 @@
 import '@testing-library/jest-dom';
-import * as ResizeObserverModule from 'resize-observer-polyfill';
 
 //! To prevent window.matchMedia is not a function error when testing with AntD components
-export default global.matchMedia =
-  global.matchMedia ||
-  function (): unknown {
-    return { matches: false, addListener: jest.fn(), removeListener: jest.fn() };
-  };
+import 'matchmedia-polyfill';
+import 'matchmedia-polyfill/matchMedia.addListener';
 
-// TODO is there a better way to do this?
 //! To prevent window.ResizeObserver is not a constructor error when testing with recharts
+import * as ResizeObserverModule from 'resize-observer-polyfill';
 global.ResizeObserver = ResizeObserverModule.default;
