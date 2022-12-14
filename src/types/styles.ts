adc0c1564d6ade64320ac47c0fa21a6f04a9a5c0
Jake Readman <jakereadman@gmail.com>
2022-12-14 20:00:39 +0000

    gloabal scss file

---
+++ src/types/styles.ts
@@ -0,0 +1,9 @@
+export enum ColourPalette {
+  ngtLime = '#14a2a5',
+  ngtAqua = '#58ccdb',
+  ngtWhite = '#FFF',
+  ngtGreen = '#3b096c',
+  ngtTeal = '#3d9970',
+  ngtMidBlue = '#19175a',
+  ngtLightBlue = '#6499c9',
+}
