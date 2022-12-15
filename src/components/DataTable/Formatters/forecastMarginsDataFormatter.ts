edb425a4ee4162aa64f584ad2a3e1fda2d23dfaf
Jake Readman <jakereadman@gmail.com>
2022-12-15 08:18:00 +0000

    data formatters folder

---
+++ src/components/DataTable/Formatters/forecastMarginsDataFormatter.ts
@@ -0,0 +1,26 @@
+import forecastMargins from '../../../data/forecastMargins.json';
+
+// eslint-disable-next-line no-console
+console.log(forecastMargins);
+
+// interface DataType {
+
+// }
+
+// const {
+//   mntriggerdemandforecastweekly: { header, mnData, demandData },
+// } = forecastMargins;
+
+// const headers = header.map((cell, index) => {
+//   return {
+//     title: cell,
+//     dataIndex: index + 1,
+//   };
+// });
+
+// const getFormattedData = () => {
+//   const rowOne = mnData.reduce((result, item, index) => {
+//     result[index + 1] = item;
+//     return result;
+//   }, {});
+// };
---
+++ src/pages/StatusView/Tables/ForecastMargins.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const ForecastMargins: FC = () => {
+  return <div>ForecastMargins</div>;
+};
+
+export default ForecastMargins;
