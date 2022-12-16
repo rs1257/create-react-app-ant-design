80e5275035fc8b0510d2fb0ccfeb7bb76a3f5d21
Ryan Smith <ryan.smith1@bp.com>
2022-12-16 15:00:55 +0000

    Add msw to mock unit tests and api requests to enable local development

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+async function main(): Promise<void> {
+  if (process.env.REACT_APP_ENV === 'development') {
+    const { worker } = await import('./mocks/browser');
+    await worker.start();
+  }
+
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+main().catch((err: Error) => err);
