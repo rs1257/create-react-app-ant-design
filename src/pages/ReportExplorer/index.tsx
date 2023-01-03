d48b659706b9d939092661a20998daee153ac42e
Alex White <alex.a.white@capgemini.com>
2023-01-03 15:48:21 +0000

    Add date picker and buttons to report explorer page

--- src/pages/ReportExplorer/index.tsx
+++ src/pages/ReportExplorer/index.tsx
@@ -1,13 +1,37 @@
 import { useState } from 'react';
-import DateRangePicker from '../../components/DateRangePicker';
+import CustomButton from '../../components/CustomButton';
+import DatePicker from '../../components/DatePicker';
 import './ReportExplorer.scss';
 
 const ReportExplorer = (): JSX.Element => {
-  const [dateRange, setDateRange] = useState<[string, string] | null>();
+  const [date, setDate] = useState<string | null>();
   return (
     <div className="report-explorer">
       Report Explorer
-      <DateRangePicker dateRange={dateRange} setDateRange={setDateRange} />
+      <div className="form">
+        <div className="date-picker">
+          <span>Gas Day</span>
+          <DatePicker date={date} setDate={setDate} />
+        </div>
+        <div className="buttons">
+          <div className="view-report-button">
+            <CustomButton
+              type="primary"
+              size="middle"
+              onClick={(): void => {
+                return;
+              }}
+            >
+              View report
+            </CustomButton>
+          </div>
+          <div className="download-buttons">
+            <button>CSV</button>
+            <button>XML</button>
+            <button>JSON</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
