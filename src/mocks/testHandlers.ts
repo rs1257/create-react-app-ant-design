38294b3808b77bb4c1fbd279bb9300f410c4eff9
Jake Readman <jakereadman@gmail.com>
2023-01-04 10:52:08 +0000

    comment addressing

--- src/mocks/testHandlers.ts
+++ src/mocks/testHandlers.ts
@@ -1,19 +1,25 @@
+import { Method } from 'axios';
 import { RestHandler, MockedRequest, DefaultBodyType, rest } from 'msw';
 
 export const setupFailedNetworkRequest = (
   url: string,
-  statusCode: number
+  statusCode: number,
+  method: Method = 'GET'
 ): RestHandler<MockedRequest<DefaultBodyType>> => {
-  return rest.get(url, (req, res, ctx) => {
-    return res(ctx.status(statusCode));
-  });
-};
-
-export const setupFailedNetworkPostRequest = (
-  url: string,
-  statusCode: number
-): RestHandler<MockedRequest<DefaultBodyType>> => {
-  return rest.post(url, (req, res, ctx) => {
-    return res(ctx.status(statusCode));
-  });
+  switch (method) {
+    case 'POST':
+      return rest.post(url, (req, res, ctx) => res(ctx.status(statusCode)));
+    case 'GET':
+      return rest.get(url, (req, res, ctx) => res(ctx.status(statusCode)));
+    case 'DELETE':
+      return rest.delete(url, (req, res, ctx) => res(ctx.status(statusCode)));
+    case 'PATCH':
+      return rest.patch(url, (req, res, ctx) => res(ctx.status(statusCode)));
+    case 'PUT':
+      return rest.put(url, (req, res, ctx) => res(ctx.status(statusCode)));
+    case 'OPTIONS':
+      return rest.options(url, (req, res, ctx) => res(ctx.status(statusCode)));
+    default:
+      throw new Error(`Method '${method}' not found`);
+  }
 };
