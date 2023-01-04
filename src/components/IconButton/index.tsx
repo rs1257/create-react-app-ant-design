77cf37cb34780440c56102229767f01d126f38f1
Alex White <alex.a.white@capgemini.com>
2023-01-04 14:25:12 +0000

    Use AntD buttons in IconButton

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,11 +1,21 @@
+import { Button } from 'antd';
 import { IconButtonProps } from '../../types/props';
 import './IconButton.scss';
 
 const IconButton = ({ name }: IconButtonProps): JSX.Element => {
   return (
-    <a className="download-link" title={`Download ${name}`}>
-      <img src={`/icon-${name}.png`} className="download-icon" />
-    </a>
+    <Button
+      className="download-button"
+      icon={
+        <img
+          src={`/icon-${name}.png`}
+          className="download-button__icon"
+          onClick={(): void => {
+            return;
+          }}
+        />
+      }
+    ></Button>
   );
 };
 
