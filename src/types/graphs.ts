a217bf9aac6f6275e062475a28e92b42be6876ff
Jake Readman <jakereadman@gmail.com>
2023-01-05 09:48:28 +0000

    hooks types moved to types files

--- src/types/graphs.ts
+++ src/types/graphs.ts
@@ -1,3 +1,24 @@
+import { Payload } from 'recharts/types/component/DefaultLegendContent';
+
 export interface Graph {
   name: string;
+}
+
+//* Hooks
+export interface Lines {
+  [key: string]: boolean;
+}
+
+export interface InteractiveLegend {
+  lines: Lines;
+  hover: string | undefined;
+  selectLine: (data: Payload) => void;
+  handleLegendMouseLeave: () => void;
+  handleLegendMouseEnter: (data: Payload) => void;
+}
+
+//* Tests
+
+export interface InteractiveLegendTestComponentProps {
+  payload?: { value: string | number | boolean };
 }
