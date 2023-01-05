5e91a4ae80ff99e2b08c44856859e5e8e7120fa7
Alex White <alex.a.white@capgemini.com>
2023-01-05 10:13:28 +0000

    Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from 'react';
 import { Layout, Menu, MenuProps } from 'antd';
 import navRoutes from '../../config/nav-routes';
 import { useNavigate } from 'react-router-dom';
-import './Navbar.scss';
+import styles from './Navbar.module.scss';
 
 const { Header } = Layout;
 
@@ -14,10 +14,10 @@ const Navbar = (): ReactElement => {
   };
 
   return (
-    <Header className="navbar">
+    <Header className={styles.navbar}>
       <Menu
         mode="horizontal"
-        className="navbar__menu"
+        className={styles.menu}
         defaultSelectedKeys={['2']}
         onClick={handleNavigation}
         items={navRoutes?.map(({ label, key }) => {
