8643aabb52e0ede41511f8c00b25995a52168bb7
Alex White <alex.a.white@capgemini.com>
2023-01-10 09:42:37 +0000

    Move form into seperate component and migrate styles and tests over

--- src/pages/DataItemExplorer/__test__/DataItemExplorer.test.tsx
+++ src/pages/DataItemExplorer/__test__/DataItemExplorer.test.tsx
@@ -1,9 +1,7 @@
-import { render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { client } from '../../../mockQueryClient';
 import DataItemExplorer from '..';
-import { getDefaultDateRange } from '../../../utils/dateTime';
-import userEvent from '@testing-library/user-event';
 
 describe('DataItemExplorer component', () => {
   it('should display DataItemExplorer component when rendered', () => {
@@ -14,96 +12,5 @@ describe('DataItemExplorer component', () => {
     );
 
     expect(queryByText('Data Item Explorer')).toBeInTheDocument();
-  });
-
-  it('should display time in the DateRangePicker if ApplicableAt is selected', async () => {
-    const { findByPlaceholderText, queryByLabelText } = render(
-      <QueryClientProvider client={client}>
-        <DataItemExplorer />
-      </QueryClientProvider>
-    );
-
-    const [defaultStartDate, defaultEndDate] = getDefaultDateRange();
-    const startDate = (await findByPlaceholderText('Start date')) as HTMLInputElement;
-    const endDate = (await findByPlaceholderText('End date')) as HTMLInputElement;
-
-    const radioApplicableAt = queryByLabelText('Applicable At') as HTMLElement;
-    expect(radioApplicableAt).toBeInTheDocument();
-    await waitFor(() => {
-      userEvent.click(radioApplicableAt);
-    });
-
-    expect(startDate.value).toEqual(defaultStartDate);
-    expect(endDate.value).toEqual(defaultEndDate);
-  });
-
-  it('should NOT display time in the DateRangePicker if ApplicableFor is selected', async () => {
-    const { findByPlaceholderText, queryByLabelText } = render(
-      <QueryClientProvider client={client}>
-        <DataItemExplorer />
-      </QueryClientProvider>
-    );
-
-    const [defaultStartDate, defaultEndDate] = getDefaultDateRange();
-    const defaultStartDateNoTime = defaultStartDate.split(' ')[0];
-    const defaultEndDateNoTime = defaultEndDate.split(' ')[0];
-
-    const startDate = (await findByPlaceholderText('Start date')) as HTMLInputElement;
-    const endDate = (await findByPlaceholderText('End date')) as HTMLInputElement;
-
-    const radioApplicableFor = queryByLabelText('Applicable For') as HTMLElement;
-    expect(radioApplicableFor).toBeInTheDocument();
-    await waitFor(() => {
-      userEvent.click(radioApplicableFor);
-    });
-
-    expect(startDate.value).toEqual(defaultStartDateNoTime);
-    expect(endDate.value).toEqual(defaultEndDateNoTime);
-  });
-
-  it('should submit NORMALDAY and latestValues successfully', async () => {
-    const { getByRole } = render(
-      <QueryClientProvider client={client}>
-        <DataItemExplorer />
-      </QueryClientProvider>
-    );
-
-    const latestValues = getByRole('checkbox');
-    const submitButton = getByRole('button', { name: 'View Data for Data Items' });
-
-    await waitFor(() => {
-      userEvent.click(latestValues);
-    });
-
-    expect(latestValues).toHaveProperty('checked', true);
-
-    await waitFor(() => {
-      userEvent.click(submitButton);
-    });
-
-    // expect submitted values to equal form values
-  });
-
-  it('should submit GASDAY and NOT latestValues successfully', async () => {
-    const { getByRole, queryByLabelText } = render(
-      <QueryClientProvider client={client}>
-        <DataItemExplorer />
-      </QueryClientProvider>
-    );
-
-    const radioApplicableFor = queryByLabelText('Applicable For') as HTMLElement;
-    expect(radioApplicableFor).toBeInTheDocument();
-
-    await waitFor(() => {
-      userEvent.click(radioApplicableFor);
-    });
-
-    const submitButton = getByRole('button', { name: 'View Data for Data Items' });
-
-    await waitFor(() => {
-      userEvent.click(submitButton);
-    });
-
-    // expect submitted values to equal form values
   });
 });
--- src/pages/DataItemExplorer/index.tsx
+++ src/pages/DataItemExplorer/index.tsx
@@ -1,83 +1,14 @@
-import { useState } from 'react';
-import { Form, Radio, Checkbox, Button, RadioChangeEvent } from 'antd';
-import dayjs from 'dayjs';
 import DataItemExplorerFolderStructure from '../../components/DataItemExplorerFolderStructure';
-import IconButton from '../../components/IconButton';
+import DataItemExplorerForm from './Form/DataItemExplorerForm';
 import styles from './DataItemExplorer.module.scss';
-import DateRangePicker from '../../components/DateRangePicker';
-import { SoapRequestBoolean, SoapRequestDateType } from '../../types/api';
-import { getDefaultDateRange } from '../../utils/dateTime';
-import { Applicable, DataItemExplorerFormData } from '../../types/data';
 
 const DataItemExplorer = (): JSX.Element => {
-  const [applicable, setApplicable] = useState<Applicable>('applicableAt');
-  const [dateRange, setDateRange] = useState<[string, string]>(getDefaultDateRange());
-  const [form] = Form.useForm<DataItemExplorerFormData>();
-
-  const onFinish = ({ latestValues, applicable }: DataItemExplorerFormData): void => {
-    //TODO if gasday (applicableFor) then change fromDate time to 4am and toDate time to 3:59am
-    const fromDate = dayjs(dateRange[0]).format('YYYY-MM-DDTHH:mm:ss');
-    const toDate = dayjs(dateRange[1]).format('YYYY-MM-DDTHH:mm:ss');
-
-    const dateType =
-      applicable === 'applicableFor' ? SoapRequestDateType.gas : SoapRequestDateType.normal;
-
-    const latestFlag = latestValues ? SoapRequestBoolean.true : SoapRequestBoolean.false;
-
-    // eslint-disable-next-line no-console
-    console.log(latestFlag, fromDate, toDate, dateType);
-  };
-
-  const applicableOnChange = (values: RadioChangeEvent): void => {
-    setApplicable(values.target.value as Applicable);
-  };
-
   return (
     <div className={styles.container}>
       <span>Data Item Explorer</span>
       <DataItemExplorerFolderStructure />
       <span>Criteria</span>
-      <Form
-        form={form}
-        onFinish={onFinish}
-        initialValues={{
-          latestValues: false,
-          applicable: 'applicableAt',
-        }}
-      >
-        <div className={styles.formControls}>
-          <div>
-            <Form.Item className={styles.latestValues} name="latestValues" valuePropName="checked">
-              <Checkbox>Latest Values</Checkbox>
-            </Form.Item>
-            <Form.Item name="applicable">
-              <Radio.Group className={styles.applicable} onChange={applicableOnChange}>
-                <Radio value="applicableAt">Applicable At</Radio>
-                <Radio value="applicableFor">Applicable For</Radio>
-              </Radio.Group>
-            </Form.Item>
-          </div>
-          <Form.Item name="datePicker">
-            <DateRangePicker
-              dateRange={dateRange}
-              setDateRange={setDateRange}
-              showTime={applicable === 'applicableAt'}
-            />
-          </Form.Item>
-        </div>
-        <div className={styles.buttonsContainer}>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              View Data for Data Items
-            </Button>
-          </Form.Item>
-          <div>
-            <IconButton name="CSV" />
-            <IconButton name="CSV" />
-            <IconButton name="XML" />
-          </div>
-        </div>
-      </Form>
+      <DataItemExplorerForm />
     </div>
   );
 };
